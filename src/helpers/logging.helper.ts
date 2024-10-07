fbe7d57aad2d141a36b190c37171637f71fd077e
hadi esna <atomaton00@gmail.com>
2024-10-07 15:56:18 +0100

    add Simple log to the library

--- src/helpers/logging.helper.ts
+++ src/helpers/logging.helper.ts
@@ -5,72 +5,84 @@ import SendGridController, { EmailSeverity } from "../send-grid";
 let logDir = path.join(__dirname, "../../../logs");
 
 if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+    fs.mkdirSync(logDir);
 }
 enum LogType {
-  INFO = "INFO",
-  ERROR = "ERROR",
-  WARN = "WARN",
-  DEBUG = "DEBUG",
+    INFO = "INFO",
+    ERROR = "ERROR",
+    WARN = "WARN",
+    DEBUG = "DEBUG",
 }
 
 const setLogDir = (dir: string) => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-  logDir = dir;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    logDir = dir;
+};
+
+const Log = (message: string, user: string = "general", type: LogType = LogType.INFO, severity: EmailSeverity = EmailSeverity.LOW, notifyAdmin: boolean = false, logToConsole: boolean = true, listToNotify: string | string[] = []) => {
+    LogMessageToFile({
+        message,
+        user,
+        type,
+        severity,
+        notifyAdmin,
+        logToConsole,
+        listToNotify,
+    });
 };
 
 const LogMessageToFile = ({
-  message,
-  user,
-  type,
-  severity = EmailSeverity.LOW,
-  notifyAdmin = false,
-  logToConsole = true,
-  listToNotify,
+    message,
+    user,
+    type,
+    severity = EmailSeverity.LOW,
+    notifyAdmin = false,
+    logToConsole = true,
+    listToNotify,
 }: {
-  message: any;
-  user?: string;
-  type: LogType;
-  severity?: EmailSeverity;
-  notifyAdmin?: boolean;
-  logToConsole?: boolean;
-  listToNotify?: string | string[];
+    message: any;
+    user?: string;
+    type: LogType;
+    severity?: EmailSeverity;
+    notifyAdmin?: boolean;
+    logToConsole?: boolean;
+    listToNotify?: string | string[];
 }) => {
-  if (!user || user.length === 0) {
-    user = "general";
-  }
-  let errorMessage = "";
-  let stackTrace = "";
+    if (!user || user.length === 0) {
+        user = "general";
+    }
+    let errorMessage = "";
+    let stackTrace = "";
 
-  if (message instanceof Error) {
-    // If message is an Error, extract information
-    errorMessage = message.message || "";
-    stackTrace = message.stack || "";
-  } else {
-    // If message is a string, use it as is
-    errorMessage = message;
-  }
+    if (message instanceof Error) {
+        // If message is an Error, extract information
+        errorMessage = message.message || "";
+        stackTrace = message.stack || "";
+    } else {
+        // If message is a string, use it as is
+        errorMessage = message;
+    }
 
-  if (logToConsole) console.log(`${type} ${errorMessage}`);
+    if (logToConsole) console.log(`${type} ${errorMessage}`);
 
-  const currentDay = new Date().toISOString().split("T")[0];
-  const newLogDir = path.join(logDir, currentDay);
-  if (!fs.existsSync(newLogDir)) {
-    fs.mkdirSync(newLogDir);
-  }
+    const currentDay = new Date().toISOString().split("T")[0];
+    const newLogDir = path.join(logDir, currentDay);
+    if (!fs.existsSync(newLogDir)) {
+        fs.mkdirSync(newLogDir);
+    }
 
-  const logFilePath = path.join(newLogDir, `${user}.${type}.log`);
-  let logMessage = `${new Date().toISOString()}: ${type} ${errorMessage}\n`;
-  // Append stack trace if available
-  if (stackTrace) {
-    logMessage += `Stack Trace:\n${stackTrace}\n`;
-  }
-  fs.appendFileSync(logFilePath, logMessage);
+    const logFilePath = path.join(newLogDir, `${user}.${type}.log`);
+    let logMessage = `${new Date().toISOString()}: ${type} ${errorMessage}\n`;
+    // Append stack trace if available
+    if (stackTrace) {
+        logMessage += `Stack Trace:\n${stackTrace}\n`;
+    }
+    fs.appendFileSync(logFilePath, logMessage);
 
-  if (notifyAdmin) {
-    const html = `
+    if (notifyAdmin) {
+        const html = `
       <h1>${type} Log</h1>
       <p>User: ${user}</p>
       <p>Severity: ${severity}</p>
@@ -78,19 +90,19 @@ const LogMessageToFile = ({
       <p>${logMessage}</p>
     `;
 
-    SendGridController.sendAnyTemplate({
-      to: listToNotify,
-      html,
-      subject: `${user} ${type} Log`,
-      inCludeNameInSubject: true,
-    });
-  }
+        SendGridController.sendAnyTemplate({
+            to: listToNotify,
+            html,
+            subject: `${user} ${type} Log`,
+            inCludeNameInSubject: true,
+        });
+    }
 };
 
 const LoggingHelper = {
-  LogMessageToFile,
-  setLogDir,
-  LogType,
+    LogMessageToFile,
+    setLogDir,
+    LogType,
 };
 
 export default LoggingHelper;
